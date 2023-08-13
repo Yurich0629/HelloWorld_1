3aebd7bf576a7652312fdcaada63840e1596c204
Ilya Desiatow <140006151+Yurich0629@users.noreply.github.com>
2023-08-14 02:31:17 +0300

    Initial Commit

---
+++ HelloWorld_1/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  HelloWorld_1
+//
+//  Created by Илья Десятов on 14.08.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var showGreetingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        greetingLabel.isHidden.toggle()
+        showGreetingButton.layer.cornerRadius = 10
+    }
+
+    @IBAction func showGreetingButtonDidTapped(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        showGreetingButton.setTitle(
+        greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+                        for: .normal
+        )
+//        if greetingLabel.isHidden {
+//            sender.setTitle("Show greeting", for: .normal)
+//        } else {
+//            sender.setTitle("Hide greeting", for: .normal)
+//        }
+    }
+    
+}
